d2887772b5c6795008f9345f267bb3523b32ed27
Lakshmi Vara Prasad Kuna <43074474+klvp@users.noreply.github.com>
2022-07-08 19:31:38 +0530

    removed production server code

--- server.js
+++ server.js
@@ -22,12 +22,6 @@ app.use("/api/users", userRoutes);
 app.use("/api/bookings", bookingRouter);
 app.use("/api/bikes", bikeRouter);
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-  });
-}
 
 app.listen(port, () => console.log(`Node Server listening on port ${port}`));
 
